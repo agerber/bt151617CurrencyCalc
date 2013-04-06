4ba0c12f97b0452af53f760e059e73c32d05b7ea
Adam Gerber <adam.gerber@yahoo.com>
2013-04-05 21:43:18 -0500

    p0

--- src/edu/uchicago/cs/gerber/hw2/Splash.java
+++ src/edu/uchicago/cs/gerber/hw2/Splash.java
@@ -18,24 +18,7 @@ public class Splash extends Activity {
 		super.onCreate(icicle);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.splashscreen);
-
-		/*
-		 * New Handler to start the Menu-Activity and close this Splash-Screen
-		 * after some seconds.
-		 */
-		new Handler().postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				/* Create an Intent that will start the Menu-Activity. */
-				Intent mainIntent = new Intent(Splash.this, Main.class);
-				startActivity(mainIntent);
-
-				/*
-				 * Once Main is popped off the stack, finish this activity
-				 * immediately
-				 */
-				Splash.this.finish();
-			}
-		}, SPLASH_DISPLAY_LENGHT);
 	}
+
+		
 }
